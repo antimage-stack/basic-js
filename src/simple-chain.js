10046fdba3d390d331d878a6525fff17c2d3c4b0
antimage-stack <vanua.osipovporg@gmail.com>
2021-03-15 15:43:47 +0300

    docs: finish with simple chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,32 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  arr: [],
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.arr.length
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.arr.push('( ' + value + ' )')
+    return this
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if ( (position ^ 0) === position && position > 0 )  {
+      this.arr.splice( position - 1, 1)
+      return this      
+    }
+    else {
+      this.arr = []
+      throw new Error()
+    }
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.arr.reverse()
+    return this
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    let str = this.arr.slice(0)
+    this.arr = []
+    return str.join('~~')
   }
 };
 
