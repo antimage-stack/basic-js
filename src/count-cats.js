131e3e56c790083c4498230981762b34ed2fac36
antimage-stack <vanua.osipovporg@gmail.com>
2021-03-15 15:33:54 +0300

    docs: finish with count-cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,17 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(matrix ) { 
+  let number = 0
+
+  for ( let i = 0; i < matrix.length; i++ ) {
+      for ( j = 0; j < matrix[i].length; j++ ) {
+          let str = '^^'
+          
+          if ( matrix[i][j] === str ) {
+              number++
+          }
+      }
+  }
+
+  return number 
 };
