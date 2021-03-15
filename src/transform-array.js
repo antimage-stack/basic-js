7af6b4913550f83375f71d13f52e2ce6fe3eb794
antimage-stack <vanua.osipovporg@gmail.com>
2021-03-15 15:45:10 +0300

    docs: finish with transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,49 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform( arr ) {
+  let arr = array.concat()
+
+  if( Array.isArray === false ) {
+    throw new Error()
+  }
+
+  for ( let i = 0; i < arr.length; i++ ) {
+    if ( arr[i] === '--discard-next' ) {
+      if ( i === arr.length ) arr.splice(i, 1)
+      else {
+        arr[i] = 'qwert'
+        arr[i + 1] = 'qwert'
+      }
+    }
+
+    else if ( arr[i] === '--discard-prev' ) {
+      if ( i === 0 ) arr.splice(i, 1)
+      else {
+        arr[i] = 'qwert'
+        arr[i - 1] = 'qwert'
+      }
+    }
+
+    else if ( arr[i] === '--double-prev' ) {
+      if ( i === 0 ) arr.splice(i, 1)
+      else {
+        arr[i] = arr[i - 1]
+      }
+    }
+
+    else if ( arr[i] === '--double-next' ) {
+      if ( i === arr.length - 1 ) arr.splice(i, 1)
+      else {
+        arr[i] = arr[i + 1]
+      }
+    }
+  }
+  
+  let res = []
+
+  for ( let item of arr ) {
+    if ( item === 'qwert' ) continue
+    res.push(item)
+  }
+  return res
 };
