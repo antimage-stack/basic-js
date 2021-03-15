5da2e08e42a01c40a560d96933b66ac5a9b72f84
antimage-stack <vanua.osipovporg@gmail.com>
2021-03-15 15:50:41 +0300

    docs: finish with what-season

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason( date ) {
+  if (!date) return 'Unable to determine the time of year!'
+    
+  try {
+      date.getTime()
+  }   catch (e) {
+          throw new Error(e)
+  }
+  
+  let month = date.getMonth()
+
+  if ( month > 1 && month <= 4) return 'spring'
+  else if ( month > 4 && month <= 7 ) return 'summer'
+  else if ( month > 7 && month <= 10 ) return 'autumn'
+  else if ( month === 11 || month <= 1 ) return 'winter'
 };
