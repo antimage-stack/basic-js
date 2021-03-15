8a8c9eb8a63234c42277721be80b6d41c8bf557d
antimage-stack <vanua.osipovporg@gmail.com>
2021-03-15 15:36:43 +0300

    docs: finish with dream team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,22 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam( members ) {
+  let str = []
+  
+  if ( !Array.isArray(members) ) return false
+
+  for ( let item of members ) {
+    if ( typeof(item) === typeof('') ) {
+      str = str.concat(item)
+    }
+  }
+
+  for ( let i = 0; i < str.length; i++ ) {
+    if ( str[i][0] === ' ' ) {
+      str[i] = str[i].slice(1)
+      i = -1
+    }
+  }
+
+  return str.map( item => item[0].toUpperCase()).sort().join('')
 };
