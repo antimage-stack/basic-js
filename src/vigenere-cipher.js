b30c10b2b5745a031bf988c92be3a0d358fadc2d
antimage-stack <vanua.osipovporg@gmail.com>
2021-03-15 15:50:16 +0300

    docs: finish with vigenere cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,101 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(type) {
+      this.type = type
+  }
+
+  encrypt(message, key) {
+      if (!message || !key ) {
+          throw new Error()
+      }
+
+      message = message.toUpperCase()
+      key = key.toUpperCase()
+      let Skey = ''
+      let Smessage = ''
+      let alphabet = ''
+    
+      //alphabet create
+      for ( let i = 65; i <= 90; i++ ) {
+        alphabet += String.fromCodePoint(i)
+      }
+    
+      //create Skey
+      let j = 0
+      for ( let i = 0 ; Skey.length < message.length; i++ ) {
+        if( message[i].codePointAt(0) >= 65 && message[i].codePointAt(0) <= 90 ) {
+          Skey += key[j]
+          j++
+          if ( j === key.length ) j = 0
+        }
+        else {
+          Skey += message[i]
+        }
+      } 
+    
+      //create Smessage
+      for ( let i = 0; i < message.length; i++ ) {
+        if( message[i].codePointAt(0) >= 65 && message[i].codePointAt(0) <= 90 ) {
+          if ( alphabet.indexOf(message[i]) + alphabet.indexOf(Skey[i]) > 25) {
+            Smessage += String.fromCodePoint( alphabet.indexOf(message[i]) + alphabet.indexOf(Skey[i]) -26 + 65)
+          }
+          else {
+            Smessage += String.fromCodePoint( alphabet.indexOf(message[i]) + alphabet.indexOf(Skey[i]) + 65)
+          }
+        }
+        else Smessage += message[i]
+      }
+
+      if ( this.type === false ) return Smessage.split('').reverse().join('')
+
+      return Smessage
   }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  decrypt(message, key) {
+      if (!message || !key ) {
+          throw new Error()
+      }
+
+      message = message.toUpperCase()
+      key = key.toUpperCase()
+      let Skey = ''
+      let Smessage = ''
+      let alphabet = ''
+    
+      //alphabet create
+      for ( let i = 65; i <= 90; i++ ) {
+        alphabet += String.fromCodePoint(i)
+      }
+    
+      //create Skey
+      let j = 0
+      for ( let i = 0 ; Skey.length < message.length; i++ ) {
+        if( message[i].codePointAt(0) >= 65 && message[i].codePointAt(0) <= 90 ) {
+          Skey += key[j]
+          j++
+          if ( j === key.length ) j = 0
+        }
+        else {
+          Skey += message[i]
+        }
+      } 
+    
+      //create Smessage
+      for ( let i = 0; i < message.length; i++ ) {
+        if( message[i].codePointAt(0) >= 65 && message[i].codePointAt(0) <= 90 ) {
+          if ( alphabet.indexOf(message[i]) - alphabet.indexOf(Skey[i]) < 0) {
+            Smessage += String.fromCodePoint( alphabet.indexOf(message[i]) - alphabet.indexOf(Skey[i]) + 26 + 65)
+          }
+          else {
+            Smessage += String.fromCodePoint( alphabet.indexOf(message[i]) - alphabet.indexOf(Skey[i]) + 65)
+          }
+        }
+        else Smessage += message[i]
+      }
+
+      if ( this.type === false ) return Smessage.split('').reverse().join('')
+
+      return Smessage
   }
 }
 
