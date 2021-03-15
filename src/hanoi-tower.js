e5fa54d7e8ec0d68c2ec0e6bffaaf687ac8322b6
antimage-stack <vanua.osipovporg@gmail.com>
2021-03-15 15:39:39 +0300

    docs: finish with hanoi tower

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,6 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function calculateHanoi( disksNumber, turnsSpeed ) {
+  let result = {
+    turns: 1,
+    seconds: 1
+  }
+
+  for ( let i = 2; i <= disksNumber; i++ ) {
+    result.turns *= 2
+    result.turns += 1
+  }
+
+  turnsSpeed = turnsSpeed / 3600
+  result.seconds = Math.floor(result.turns / turnsSpeed)
+
+  return result
 };
